32ccda491a4fb4f10c68b284844f79c5c707a0ec
anketmanoj <anket.m@live.com>
2021-08-16 16:47:22 +0400

    start video part 17 at 8.20

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sell_now_vendor/screens/loginScreen.dart';
 import 'package:sell_now_vendor/screens/registerScreen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,7 +14,7 @@ class HomeScreen extends StatelessWidget {
         child: ElevatedButton(
           onPressed: () {
             FirebaseAuth.instance.signOut();
-            Navigator.pushReplacementNamed(context, RegistrationScreen.id);
+            Navigator.pushReplacementNamed(context, LogoinScreen.id);
           },
           child: Text("Sign out"),
         ),
